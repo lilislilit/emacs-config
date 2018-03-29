bb77e8581245a6208ac1a543e3a45d5ae0935f63
Pavel Zeppa <pavel.zepp@gmail.com>
2018-03-29 20:11:22 +0300

    flyspell config updated

--- conf.d/fs.el
+++ conf.d/fs.el
@@ -1,7 +1,4 @@
-(setq
- flyspell-delay 1
- flyspell-default-dictionary ispell-dictionary)
-
 (define-key flyspell-mode-map (kbd "C-c i") #'ispell-change-dictionary)
 
-(add-hook 'text-mode-hook #'flyspell-mode)
+(add-hook 'text-mode-hook #'(lambda () (flyspell-mode 1)))
+(add-hook 'org-mode-hook #'(lambda () (flyspell-mode 1)))
