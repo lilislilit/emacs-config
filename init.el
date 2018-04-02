e02e736d658c4f18cedbd292fcb3362c8113fabd
Pavel Zeppa <pavel.zepp@gmail.com>
2018-04-02 21:22:48 +0300

    meghanada config

--- init.el
+++ init.el
@@ -157,6 +157,8 @@ various buffer management routines")
               :config "helm")
 (load-package 'helm-gtags :config "helm-gtags")
 
+(load-package 'meghanada :config "megha")
+
 ;; embedded packages
 (load-package 'ispell :config "spell")
 (load-package 'flyspell :config "fs" :required t)
